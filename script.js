026cdcf0eb9a6b9ec94796150a1486137b5c5732
ben-rainer <brainer8210@gmail.com>
2025-10-06 18:33:04 -0400

    Added base HTML for the features page, a test css file and navbar and footer files

---
+++ script.js
@@ -0,0 +1,20 @@
+function init() {
+    // Initializes the page by adding global code (the navigation bar and the footer)
+    let navbar_container = document.getElementById("navbar");
+    let footer_container = document.getElementById("footer");
+    if(navbar_container) {
+        fetch("/navigation.html").then((resp) => resp.text()).then((htmltext) => {
+            navbar_container.innerHTML = htmltext;
+        })
+    }
+
+    if(footer_container) {
+        fetch("/footer.html").then((resp) => resp.text()).then((htmltext) => {
+            footer_container.innerHTML = htmltext;
+        })
+    }
+}
+
+document.body.onload = function(){
+    init();
+};
