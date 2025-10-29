f5c5b3deff99cb6be4ebd895429840aadf401ce3
Najib <Najibmauthoor@gmail.com>
2025-10-28 21:28:30 -0400

    adding button click id

--- script.js
+++ script.js
@@ -23,3 +23,13 @@ function toggleMenu() {
     let menubar = document.querySelector(".menu-content");
     menubar.classList.toggle("visible");
 }
+
+function openPopup() {
+      document.getElementById("popup").style.display = "block";
+      document.getElementById("overlay").style.display = "block";
+    }
+
+function closePopup() {
+      document.getElementById("popup").style.display = "none";
+      document.getElementById("overlay").style.display = "none";
+    }
