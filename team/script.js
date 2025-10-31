157bcdf96957d0ffcce7fec3ebb8e5de6c44adb1
Najib Mauthoor <najibmauthoor@gmail.com>
2025-10-31 18:47:13 -0400

    Finishing up team memebers page

--- team/script.js
+++ team/script.js
@@ -1,41 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Sample team member data - replace with your actual team members
     const teamMembers = {
         'member1': {
             name: 'Austin Koola',
             description: '',
-            image: '', // Add image path
-            linkedin: 'https://linkedin.com/in/member1',
-            github: 'https://github.com/member1',
-            portfolio: 'https://member1.dev'
+            image: 'wdawda', // Add image path
+            linkedin: 'https://www.linkedin.com/in/austinkoola/',
+            github: 'https://github.com/moonieweme',
         },
         'member2': {
             name: 'Ben Rainer',
             description: 'Ben Rainer is a software engineering student and web developer. He is responsible for some of the front-end development and quality assurance in CampusNav. He enjoys contributing to programming projects that solve problems and improve his community, and he is excited to be a part of the CampusNav team. In his spare time, he enjoys playing jazz trumpet.',
-            image: '', // Add image path
-            linkedin: 'https://linkedin.com/in/member2',
-            github: 'https://github.com/member2',
-            portfolio: 'https://member2.dev'
+            image: './Images/ben.jpg', 
+            linkedin: 'https://www.linkedin.com/in/benrainer/',
+            github: 'https://github.com/ben-rainer',
         },
         'member3': {
             name: 'Daniel Lieu',
             description: '',
-            image: '', // Add image path
-            linkedin: 'https://linkedin.com/in/member2',
-            github: 'https://github.com/member2',
-            portfolio: 'https://member2.dev'
+            image: 'wadawd', // Add image path
+            linkedin: 'https://www.linkedin.com/in/daniel-lieu-9514a4388/',
+            github: 'https://github.com/teikoku-dansei',
+            
         },
         'member4': {
             name: 'Fatima Kanj',
             description: '',
-            image: '', // Add image path
-            linkedin: 'https://linkedin.com/in/member2',
-            github: 'https://github.com/member2',
-            portfolio: 'https://member2.dev'
+            image: 'wdawda', // Add image path
+            linkedin: 'https://www.linkedin.com/in/fatima-k-7798a2246/',
+            github: 'https://github.com/fkanj70',
         },
         'member5': {
             name: 'Najib Mauthoor',
-            description: '',
+            description: 'Najib Mauthoor is a software engineering student and web developer. He designed the Team Page and contributed to the overall website development for CampusNav. Passionate about technology and gaming, he enjoys exploring how innovation shapes the future and continues to improve his programming skills through creative projects.',
             image: './Images/Najib.jpg', 
             linkedin: 'https://www.linkedin.com/in/najib-mauthoor-806410269/',
             github: 'https://github.com/Treforis',
@@ -51,7 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const memberDescription = overlay.querySelector('.member-description');
     const linkedinLink = overlay.querySelector('.linkedin-link');
     const githubLink = overlay.querySelector('.github-link');
-    const portfolioLink = overlay.querySelector('.portfolio-link');
 
     // Add click event listeners to all "About Me" buttons
     document.querySelectorAll('.about').forEach((button, index) => {
